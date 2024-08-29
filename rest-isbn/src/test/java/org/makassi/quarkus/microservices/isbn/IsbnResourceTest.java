ead73da03dfa44867b59580546b4d29dc3eda1a7
Brahima Traore <my123gate@gmail.com>
2024-08-28 20:53:14 -0400

    fix unit test for rest-isbn

--- rest-isbn/src/test/java/org/makassi/quarkus/microservices/isbn/IsbnResourceTest.java
+++ rest-isbn/src/test/java/org/makassi/quarkus/microservices/isbn/IsbnResourceTest.java
@@ -4,17 +4,21 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.hasKey;;
 
 @QuarkusTest
 class IsbnResourceTest {
     @Test
     void testHelloEndpoint() {
         given()
-          .when().get("/api/isbns")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+                .when().get("/api/isbns")
+                .then()
+                .statusCode(200)
+                .body("isbn_13", startsWith("13-"))
+                .body("isbn_10", startsWith("10-"))
+                .body(not(hasKey("generationDate")));
     }
 
 }
